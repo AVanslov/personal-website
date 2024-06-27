1ca0354e5926bbaa3fd75e7184b50f6a49c8e307
AVanslov <a.buchelnikov99@gmail.com>
2024-06-27 16:57:18 +0200

    Add templates

--- personalwebsite/mainpage/views.py
+++ personalwebsite/mainpage/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 
-from blog.models import Post
+# from blog.models import Post
 from portfolio.models import Project
 
 
-def main_page(request):
-    projects = Project.objects.all().order_by('-pub_date')[1:5]
-    posts = Post.objects.all().order_by('-pub_date')[1:5]
+def main(request):
+    # projects = Project.objects.all().order_by('-pub_date')[1:5]
+    # posts = Post.objects.all().order_by('-pub_date')[1:5]
 
     context = {
-        'projects': projects,
-        'posts': posts,
+        # 'projects': projects,
+        # 'posts': posts,
     }
     return render(request, 'homepage/main.html', context)
