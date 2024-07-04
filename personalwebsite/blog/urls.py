5b9d37d25d627a8e7e32e2834c0730ab774a1841
AVanslov <a.buchelnikov99@gmail.com>
2024-07-04 10:29:34 +0200

    Set details and list pages of blog and portfolio. Also post and project cards have been fixed

--- personalwebsite/blog/urls.py
+++ personalwebsite/blog/urls.py
@@ -4,5 +4,15 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
+    path(
+        'blog/<int:pk>',
+        views.PostDetailView.as_view(),
+        name='post_detail'
+    ),
+    path(
+        'blog/<slug:category_slug>/',
+        views.CategoryPostListView.as_view(),
+        name='category_posts'
+    ),
     path('blog/', views.PostListView.as_view(), name='all_posts'),
 ]
--- personalwebsite/blog/views.py
+++ personalwebsite/blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 import markdown
 
 from django.views.generic import ListView, DetailView
@@ -13,25 +14,73 @@ class PostListView(ListView):
     template_name = 'blog/post_list.html'
     context_object_name = 'page_obj'
 
-    def converter_to_markdown_content(self):
+    def converter_to_markdown_content(self, posts):
         md = markdown.Markdown(extensions=["fenced_code"])
         all_posts = []
-        for post in Post.objects.order_by('pub_date').all():
+        for post in posts:
             post.text = md.convert(post.text)
             all_posts.append(post)
         return all_posts
 
     def get_queryset(self):
-        queryset = {'posts': self.converter_to_markdown_content(),
+        queryset = {'posts': self.converter_to_markdown_content(
+            posts=Post.objects.order_by('pub_date').all()
+        ),  # добавить пагинацию
                     'categories': Category.objects.order_by('name').all()}
         return queryset
 
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['posts'] = self.converter_to_markdown_content()
-    #     return context
-
-
 class PostDetailView(DetailView):
     model = Post
+    template_name = 'blog/post_detail.html'
+
+    def get_post(self):
+        return get_object_or_404(
+            Post,
+            id=self.kwargs['pk']
+        )
+
+    def converter_to_markdown_content(self):
+        post = self.get_post()
+        md = markdown.Markdown(extensions=["fenced_code"])
+        post.text = md.convert(post.text)
+        return post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post'] = self.converter_to_markdown_content()
+        return context
+
+
+class CategoryPostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'page_obj'
+
+    def converter_to_markdown_content(self, posts):
+        md = markdown.Markdown(extensions=["fenced_code"])
+        all_posts = []
+        for post in posts:
+            post.text = md.convert(post.text)
+            all_posts.append(post)
+        return all_posts
+
+    def get_category(self):
+        return get_object_or_404(
+            Category,
+            slug=self.kwargs['category_slug']
+        )
+
+    def get_queryset(self):
+        queryset = {
+            'posts': self.converter_to_markdown_content(
+                posts=self.get_category().posts.all()
+            ),
+            'categories': Category.objects.all()
+        }
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.get_category()
+        return context
